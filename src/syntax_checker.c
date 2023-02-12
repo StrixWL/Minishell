f1ab6c4d21d35437023eba5c7a370fce700dc282
StrixWL <brahimstrix@gmail.com>
2023-02-12 15:18:23 +0100

    Updated syntax checker

--- src/syntax_checker.c
+++ src/syntax_checker.c
@@ -6,7 +6,7 @@
 /*   By: bel-amri <clorensunity@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 20:06:06 by bel-amri          #+#    #+#             */
-/*   Updated: 2023/02/12 14:35:11 by bel-amri         ###   ########.fr       */
+/*   Updated: 2023/02/12 15:16:51 by bel-amri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,18 @@ t_bool	check_for_pipe(t_token *token)
 	return (TRUE);
 }
 
+t_bool	check_for_redir(t_token *token)
+{
+	t_token	*p;
+
+	p = token->next;
+	while (p && p->type == WSPACE)
+		p = p->next;
+	if (!(p && (p->type == WORD || p->type == VAR)))
+		return (FALSE);
+	return (TRUE);
+}
+
 /* bullshit code bruh */
 t_bool	syntax_check(t_token *tokens)
 {
@@ -43,6 +55,9 @@ t_bool	syntax_check(t_token *tokens)
 	{
 		if (tokens->type == PIPE)
 			if (!check_for_pipe(tokens))
+				return (FALSE);
+		if (is_redirection(tokens->type))
+			if (!check_for_redir(tokens))
 				return (FALSE);
 		tokens = tokens->next;
 	}
